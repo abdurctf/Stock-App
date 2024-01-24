ea1bf4e470f6a1b79cdf18bc5ca3e958744f684a
abdurctf <asharif1@usf.edu>
2024-01-24 09:49:40 -0500

    Add project files.

---
+++ COP 4365 002 Spring 2024 Demo/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace COP_4365_002_Spring_2024_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog_TickerChooser = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(707, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pick a Stock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog_TickerChooser
+            // 
+            this.openFileDialog_TickerChooser.FileName = "openFileDialog1";
+            this.openFileDialog_TickerChooser.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_ticker_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1576, 1081);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Please choose a stock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog_TickerChooser;
+    }
+}
+
---
+++ COP 4365 002 Spring 2024 Demo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COP_4365_002_Spring_2024_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        openFileDialog_TickerChooser.ShowDialog();
+        }
+
+        private void openFileDialog_ticker_FileOk(object sender, CancelEventArgs e)
+        {
+            string filename = openFileDialog_TickerChooser.FileName;
+            this.Text = filename;
+            //read the filename
+            goReadFile(filename);
+        }
+
+        private void goReadFile(string filename)
+        {
+            throw new NotImplementedException();
+            //read the file implementation
+        }
+    }
+}
